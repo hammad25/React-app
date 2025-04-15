13d510e4f9d4d0605d9577d605237a8dedf78b92
hammad25 <hammad2091504@hotmail.com>
2025-04-15 15:49:34 +0100

    add button component and pass children

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -7,12 +8,20 @@ function App() {
   //   console.log(item);
   // };
 
+  const handleClick = () => {
+    console.log("Button clicked");
+  };
+
   return (
     <div>
       {/* <ListGroup items={items} heading="Cities" onSelectitem={handleSelectItem} /> */}
       <Alert>
         Hello <span>World</span>
       </Alert>
+      <Button onClick={handleClick} color="success">
+        My Button
+        <p>Im a child</p>
+      </Button>
     </div>
   );
 }
