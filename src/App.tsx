86eaa570fd303c4e1274cea859f2d79f1d6c61bb
hammad25 <hammad2091504@hotmail.com>
2025-04-15 15:14:55 +0100

    pass children via pros

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectitem={handleSelectItem} />
-      <Alert />
+      {/* <ListGroup items={items} heading="Cities" onSelectitem={handleSelectItem} /> */}
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
